5f4bded44aef5438d7c39a589468904515488aa6
deinemuttermitbutter <126014902+deinemuttermitbutter@users.noreply.github.com>
2025-03-15 20:01:14 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
